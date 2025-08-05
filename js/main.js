f3708a165e1758c596c9d55be277176e3ef50eb7
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-08-06 05:11:13 +0600

    Fully functional responsive input

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 // Event listener to show mobile menu options
 let menuBtn = document.querySelector(".menu-btn");
-let navOptions2 = document.querySelector(".nav-options2");
+let searchIconBtn = document.querySelector(".search-icon-btn");
 let crossBtn = document.querySelector(".cross");
+let backBtn = document.querySelector(".back-btn");
+let logo = document.querySelector(".logo");
+let navOptions2 = document.querySelector(".nav-options2");
+let searchBox = document.querySelector(".search-box");
+let mobileSpecialNav = document.querySelector(".mobile-special-nav");
 
 menuBtn.addEventListener("click", () => {
     navOptions2.style.transform = "translateX(0%)";
@@ -9,4 +14,16 @@ menuBtn.addEventListener("click", () => {
 
 crossBtn.addEventListener("click", () => {
     navOptions2.style.transform = "translateX(-100%)";
+})
+
+searchIconBtn.addEventListener("click", () => {
+    searchBox.style.display = "flex";
+    searchBox.style.gap = "10px"
+    searchBox.style.justifyContent = "start";
+    mobileSpecialNav.style.display = "none";
+})
+
+backBtn.addEventListener("click", () => {
+    searchBox.style.display = "none";
+    mobileSpecialNav.style.display = "flex";
 })
