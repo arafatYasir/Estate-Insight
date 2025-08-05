2c799053ded485fbdb07ebb0bf1ef19994fa1b92
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-08-06 04:17:11 +0600

    Fully functional menu added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+// Event listener to show mobile menu options
+let menuBtn = document.querySelector(".menu-btn");
+let navOptions2 = document.querySelector(".nav-options2");
+let crossBtn = document.querySelector(".cross");
+
+menuBtn.addEventListener("click", () => {
+    navOptions2.style.transform = "translateX(0%)";
+})
+
+crossBtn.addEventListener("click", () => {
+    navOptions2.style.transform = "translateX(-100%)";
+})
