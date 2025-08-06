8318d4de3d871f4c3044e257010fb73d873c17e0
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-08-06 06:50:28 +0600

    Dropdonws functionality added

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ let logo = document.querySelector(".logo");
 let navOptions2 = document.querySelector(".nav-options2");
 let searchBox = document.querySelector(".search-box");
 let mobileSpecialNav = document.querySelector(".mobile-special-nav");
+let filterDropdowns = document.querySelectorAll(".custom-dropdown");
+
+// Event listener to close dropdowns on the click of outside the box
+
 
 // Responsive Navbar
 menuBtn.addEventListener("click", () => {
@@ -30,3 +34,25 @@ backBtn.addEventListener("click", () => {
 })
 
 
+// Custom Filter Dropdowns
+filterDropdowns.forEach(dropdown => {
+    const selected = dropdown.querySelector(".custom-dropdown-selected");
+    const options = dropdown.querySelector(".custom-dropdown-options");
+
+    selected.addEventListener("click", () => {
+        // Closing other dropdowns
+
+        document.querySelectorAll(".custom-dropdown").forEach(d => {
+            if(d !== dropdown) d.classList.remove("active");
+        });
+        dropdown.classList.toggle("active");
+    })
+
+    options.querySelectorAll("li").forEach(option => {
+        option.addEventListener("click", () => {
+            selected.innerHTML = option.innerHTML;
+            dropdown.classList.remove("active");
+            dropdown.setAttribute("data-selected", option.dataset.value);
+        })
+    })
+})
